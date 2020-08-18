a168b34456f2d5229cbef59f66e51fe01833b959
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-08-18 17:43:57 +0100

    server adjustments

--- server/database.js
+++ server/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const localDBName = 'fullstack-react-library';
-const MONGODB_URI = `mongodb+srv://dbUser:L0WW00w972XAVjQq@cluster0.gneug.mongodb.net/${localDBName}?retryWrites=true&w=majority`;
-
 const promise = mongoose
-  .connect(MONGODB_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true, // Googled
